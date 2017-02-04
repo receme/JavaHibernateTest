e7b417aa3e4724876ff12ce50de6442c8f86ea27
tahmid <tahmid1989@gmail.com>
2017-02-04 20:55:50 +0600

    add method to update product

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import Persistents.Order;
+import Persistents.Product;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -15,10 +16,10 @@ public class Main {
         System.out.println("2. View all orders");
         System.out.println("3. View all products");
         System.out.println("4. Add order");
-        System.out.println("4. Delete order");
-        System.out.println("5. Add product");
-        System.out.println("6. Delete product");
-        System.out.println("7. Update product");
+        System.out.println("5. Delete order");
+        System.out.println("6. Add product");
+        System.out.println("7. Delete product");
+        System.out.println("8. Update product");
         System.out.println("0. Exit");
 
         int exit = 0;
@@ -35,22 +36,66 @@ public class Main {
                         PrinterManager.printOrder(order);
 
                     } catch (Exception e) {
-                        e.printStackTrace();
+                        PrinterManager.printMessage(e.getMessage());
                     }
                     break;
                 case 2:
-
+                    try {
+                        List<Order> orders = orderController.getAllOrder();
+                        for(int i=0;i<orders.size();i++){
+                            Order order = orders.get(i);
+                            PrinterManager.printOrder(order);
+                        }
+                    } catch (Exception e) {
+                        PrinterManager.printMessage(e.getMessage());
+                    }
                     break;
                 case 3:
-
+                    try {
+                        List<Product> products = orderController.getAllProducts();
+                        PrinterManager.printAllProduct(products);
+                    } catch (Exception e) {
+                        PrinterManager.printMessage(e.getMessage());
+                    }
                     break;
                 case 4:
+                    PrinterManager.printMessage("Order details:");
+                    Order order = new Order();
+                    order.setDate(new Date().toString());
+                    PrinterManager.printMessage("How many product will be added?");
+                    int productCount = sc.nextInt();
+
+                    List<Product> products = new ArrayList<Product>();
+                    for(int i=0;i<productCount;i++){
+                        PrinterManager.printMessage("Enter product id to add:");
+                        int pId = sc.nextInt();
+                        try {
+                            Product product = orderController.getProduct(pId);
+                            products.add(product);
+                        } catch (Exception e) {
+                            PrinterManager.printMessage(e.getMessage());
+                            i--;
+                        }
+                    }
+                    order.setProducts(products);
+
+                    try {
+                        orderController.addOrder(order);
+                    } catch (Exception e) {
+                        PrinterManager.printMessage(e.getMessage());
+                    }
                     break;
                 case 5:
+                    PrinterManager.printMessage("Not implemented");
                     break;
                 case 6:
+                    PrinterManager.printMessage("Not implemented");
                     break;
                 case 7:
+                    PrinterManager.printMessage("Not implemented");
+                    break;
+                case 8:
+                    PrinterManager.printMessage("Not implemented");
                     break;
                 case 0:
                     exit = 1;
--- src/main/java/OrderController.java
+++ src/main/java/OrderController.java
@@ -1,6 +1,8 @@
 import Persistents.Order;
 import Persistents.Product;
 
+import java.util.List;
+
 /**
  * Created by receme on 2/4/17.
  */
@@ -48,6 +50,15 @@ public class OrderController {
         }
     }
 
+    public void updateProduct(Product product) throws Exception {
+        int result = serviceProvider.updateObject(product);
+        serviceProvider.closeSession();
+
+        if(result == 0){
+            throw new Exception("Error occured. Could not update product");
+        }
+    }
+
     public Order getOrder(int orderId) throws Exception {
         Order order = serviceProvider.getOrder(orderId);
         serviceProvider.closeSession();
@@ -56,5 +67,40 @@ public class OrderController {
         }
 
         return order;
+    }
+
+    public List<Order> getAllOrder() throws Exception {
+
+        List<Order> orders = serviceProvider.getAllOrder();
+        serviceProvider.closeSession();
+
+        if(orders==null){
+            throw new Exception("Error occured. Failed to fetch orders");
+        }
+
+        return orders;
+    }
+
+    public Product getProduct(int productId) throws Exception {
+        Product product = serviceProvider.getProduct(productId);
+        serviceProvider.closeSession();
+
+        if(product == null){
+            throw new Exception("Error occured. Failed to fetch product");
+        }
+
+        return product;
+    }
+
+    public List<Product> getAllProducts() throws Exception {
+
+        List<Product> products = serviceProvider.getAllProduct();
+        serviceProvider.closeSession();
+
+        if(products==null){
+            throw new Exception("Error occured. Failed to fetch products");
+        }
+
+        return products;
     }
 }
